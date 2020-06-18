a74cc323b697e977731ef1897dcaa4a000ef7360
kevin <kevinchen19888@163.com>
2020-06-18 11:23:05 +0800

    删除链表倒数第N个节点&链表相邻节点两两交换

--- src/main/java/linkedlist/LinkedListSwapPairs.java
+++ src/main/java/linkedlist/LinkedListSwapPairs.java
@@ -1,11 +1,83 @@
 package linkedlist;
 
-/**链表节点两两交换
- * 1->2->3->4
- * 2->1->4->3
+import lombok.Data;
+
+/**
+ * 给定一个链表，两两交换其中相邻的节点，并返回交换后的链表。
+ * 你不能只是单纯的改变节点内部的值，而是需要实际的进行节点交换。
+ * 题目要求：不能修改结点的 val 值，O(1) 空间复杂度。
+ * <p>
+ * 示例:
+ * 给定 1->2->3->4, 应该返回 2->1->4->3.
+ * <p>
+ * 思路:
+ * 1,设置一个偏移指针cur,从head节点开始,每隔一个节点进行移动;
+ * 2,cur指针对应节点,与链接的下一个节点进行交换,交换完毕移动cur(注意:在交换前需要提供一个临时节点变量来存储cur链接的下一个节点);
  *
  * @author kevin chen
  */
 public class LinkedListSwapPairs {
 
+    public static void main(String[] args) {
+        LinkNode head = new LinkNode(1);
+        LinkNode linkNode2 = new LinkNode(2);
+        LinkNode linkNode3 = new LinkNode(3);
+        LinkNode linkNode4 = new LinkNode(4);
+
+        head.setNext(linkNode2);
+        linkNode2.setNext(linkNode3);
+        linkNode3.setNext(linkNode4);
+        System.out.println(linkedListSwapPairs(head));
+    }
+
+    /**
+     * 使用递归
+     * <p>
+     * 关注点:
+     * 返回值：交换完成的子链表
+     * 调用单元：设需要交换的两个点为 head 和 next，head 连接后面交换完成的子链表，next 连接 head，完成交换
+     * 终止条件：head 为空指针或者 next 为空指针，也就是当前无节点或者只有一个节点，无法进行交换
+     *
+     * @param head
+     */
+    public static LinkNode linkedListSwapPairs(LinkNode head) {
+        //LinkNode cur = head;
+        //boolean pendAlter = true;
+        //while (cur != null) {
+        //    LinkNode next = cur.next;
+        //    LinkNode nextCur = cur.next;
+        //    if (next != null) {
+        //        LinkNode nextCurs = next.next;
+        //        cur.setNext(nextCurs);
+        //        next.setNext(cur);
+        //        if (pendAlter) {
+        //            head = next;
+        //            pendAlter = false;
+        //        }
+        //    }
+        //    cur = next;
+        //}
+        //return head;
+        if (head == null || head.next == null) {
+            return head;
+        }
+        // 预先存储head链接的下个节点
+        LinkNode next = head.next;
+        // 递归进行下次交换(使用head的下下个节点进行下次交换)
+        head.next = linkedListSwapPairs(next.next);
+        // 交换
+        next.next = head;
+        return next;
+    }
+
+
+    @Data
+    private static class LinkNode {
+        LinkNode next;
+        int val;
+
+        public LinkNode(int val) {
+            this.val = val;
+        }
+    }
 }
---
+++ src/main/java/linkedlist/RemoveNthFromEnd.java
@@ -0,0 +1,76 @@
+package linkedlist;
+
+import lombok.Data;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * 给定一个链表，删除链表的倒数第 n 个节点，并且返回链表的头结点。
+ * <p>
+ * 示例：
+ * 给定一个链表: 1->2->3->4->5, 和 n = 2.
+ * <p>
+ * 当删除了倒数第二个节点后，链表变为 1->2->3->5.
+ * 说明：
+ * 给定的 n 保证是有效的(0 < n < 链表链接节点数量)。
+ * <p>
+ * 思路:
+ * 1,遍历链表,将链表所链接的所有节点放入一个list中,可获得链表上节点数量count=list.size;
+ * 2,取出第count-n(倒数第n个节点)&count-n-1(待删除节点的上一个节点[注意count==n的判断)
+ * 3,判断如果count==n则将head的下一个节点返回;
+ * <p>
+ * 时间复杂度O(n)
+ * 空间复杂度O(n)
+ *
+ * @author kevin chen
+ */
+public class RemoveNthFromEnd {
+
+    public static void main(String[] args) {
+        ListNode head = new ListNode(1);
+        ListNode listNode2 = new ListNode(2);
+        ListNode listNode3 = new ListNode(3);
+        ListNode listNode4 = new ListNode(4);
+        ListNode listNode5 = new ListNode(5);
+
+        head.setNext(listNode2);
+        listNode2.setNext(listNode3);
+        listNode3.setNext(listNode4);
+        listNode4.setNext(listNode5);
+
+        System.out.println(removeNthFromEnd(head, 5));
+
+    }
+
+    public static ListNode removeNthFromEnd(ListNode head, int n) {
+        List<ListNode> list = new ArrayList<>();
+        ListNode cur = head;
+        while (cur != null) {
+            list.add(cur);
+            cur = cur.next;
+        }
+
+        int inverseN = list.size() - n;
+        // 如果倒数第n个节点的上个节点不存在
+        if (inverseN - 1 < 0) {
+            return head.next;
+        } else {
+            ListNode preNode = list.get(inverseN - 1);
+            preNode.next = list.get(inverseN).next;
+        }
+
+        return head;
+    }
+
+
+    @Data
+    private static class ListNode {
+        ListNode next;
+        int val;
+
+        public ListNode(int val) {
+            this.val = val;
+        }
+    }
+}
